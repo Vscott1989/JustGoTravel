d9ab6ef1255d1b05c527401b275fd5d77ba4c71e
vscott1989 <vscott1811@gmail.com>
2021-05-14 10:44:12 -0400

    CreatedServices

---
+++ JustGoTravel.Services/AgentService.cs
@@ -0,0 +1,59 @@
+﻿using JustGoTravel.Data;
+using JustGoTravel.Models.Agent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustGoTravel.Services
+{
+   public class AgentService
+    {
+        private readonly Guid _userId;
+        public AgentService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateAgent(AgentCreate model)
+        {
+            var entity = new Agent()
+            {
+                AuthorId = _userId,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Company = model.Company,
+                PhoneNumber = model.PhoneNumber,
+                Email = model.Email,
+                LinkedIn = model.LinkedIn
+
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Agents.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<AgentListItem> GetAgents()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .Agents
+                    .Where(e => e.AuthorId == _userId)
+                    .Select(e => new AgentListItem
+                    {
+                        FirstName = e.FirstName,
+                        LastName = e.LastName,
+                        Company = e.Company,
+                        PhoneNumber = e.PhoneNumber,
+                        Email = e.Email,
+                        LinkedIn = e.LinkedIn
+
+                    });
+                return query.ToArray();
+            }
+        }
+
+    }
+}
---
+++ JustGoTravel.Services/VacationPackService.cs
@@ -0,0 +1,62 @@
+﻿using JustGoTravel.Data;
+using JustGoTravel.Models.VacationPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustGoTravel.Services
+{
+   public class VacationPackService
+    {
+        private readonly Guid _userId;
+
+        public VacationPackService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateVacationPack(VacationPackCreate model)
+        {
+            var entity =
+                new VacationPack()
+                {
+                    AuthorId = _userId,
+                    Title = model.Title,
+                    TripLength =  model.TripLength,
+                    TotalCost = model.TotalCost,
+                    Location = model.Location,
+                    Description = model.Description,
+                    TimeOfPublication = DateTimeOffset.Now
+
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.VacationPacks.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<VacationPackListItem> GetVacationPacks()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .VacationPacks
+                    .Where(e => e.AuthorId == _userId)
+                    .Select(e => new VacationPackListItem
+                    {
+                        VacationPackID = e.ID,
+                        Title = e.Title,
+                        TripLength = e.TripLength,
+                        TotalCost = e.TotalCost,
+                        Location = e.Location,
+                        Description = e.Description,
+                        TimeOfPublication=e.TimeOfPublication
+
+
+                    });
+                return query.ToArray();
+            }
+        }
+    }
+}
